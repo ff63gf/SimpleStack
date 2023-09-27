389aa4890412a110bbefff19556532e7c7c9be2a
HungryBearN <ff8gf@naver.com>
2023-09-27 10:15:30 +0900

    add comment to main.cpp

--- MyStack/main.cpp
+++ MyStack/main.cpp
@@ -5,15 +5,21 @@ void main()
 {
 	Stack stack;
 
+	// 3개의 데이터를 삽입
 	stack.push(1);
 	stack.push(5);
 	stack.push(9);
 
+	// 현재 사이즈 = 3
 	int size = stack.size();
 
+	// pop으로 최상단 데이터 출력 및 제거
 	int n1 = stack.pop();
+
+	// top으로 최상단 데이터 출력
 	int n2 = stack.top();
 
+	// empty로 데이터 고갈 여부 확인
 	int n3 = 0;
 	while (stack.empty())
 	{
